90b6585c79a27332d43772b358aac280f6ea4335
Marion <marion.chimiack@gmail.com>
2019-07-06 23:32:05 -0700

    handle data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, {createRef, useState} from 'react';
 import './App.css';
 
+const MovieList = (props) => {
+  return (
+    <div>
+      {props.movies.map(movie => <Movie {...movie}/>)}
+    </div>
+  );
+}
 
-const Search = () => {
+const Movie = (props) => {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <p>{props.release_date}</p>
+      <p>{props.overview}</p>
+      <p>{props.vote_average}</p>
+    </div>
+  );
+}
+
+const Search = (props) => {
   const input = createRef();
   const submitSearch = async (event) => {
     event.preventDefault();
     try{
       const response = await fetch(`https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&query=${input.current.value}&language=en-US&api_key=9e9995195d663f583df605660d2316f5`);
       const data = await response.json();
-      console.log(data);
+      props.onSubmit(data.results);
     } catch (error){
       console.log(error);
     }
@@ -25,11 +43,15 @@ const Search = () => {
 }
 
 const App = (props) => {
-  const [data, setData] = useState([{ text: 'Learn Hooks' }]);
+  const [data, setData] = useState([]);
+  const generateList = (moviesData) => {
+      setData(prevState => [...prevState,...moviesData]);
+  }
   return (
     <div>
       <h1>{props.title}</h1>
-      <Search />
+      <Search onSubmit={generateList}/>
+      <MovieList movies={data}/>
     </div>
   );
 }
