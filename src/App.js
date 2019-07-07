2ee687ffcfbd0f2b90212fb60bbcb1fb5647bd81
Marion <marion.chimiack@gmail.com>
2019-07-06 22:48:35 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {createRef, useState} from 'react';
 import './App.css';
 
-function App() {
+
+const Search = () => {
+  const input = createRef();
+  const submitSearch = async (event) => {
+    event.preventDefault();
+    try{
+      const response = await fetch(`https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&query=${input.current.value}&language=en-US&api_key=9e9995195d663f583df605660d2316f5`);
+      const data = await response.json();
+      console.log(data);
+    } catch (error){
+      console.log(error);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={submitSearch}>
+        <input type="text" ref={input} placeholder="Search for something" required/>
+        <button>Search</button>
+      </form>
+    </div>
+  );
+}
+
+const App = (props) => {
+  const [data, setData] = useState([{ text: 'Learn Hooks' }]);
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <Search />
     </div>
   );
 }
